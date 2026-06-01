35817dbf24a688606028f0afa1a2d34b4eaae456
Yuni Nguyen <yuninguyen.it@gmail.com>
2026-06-02 01:56:52 +0700

    fix: set dir to __dirname in Next.js initialization to find production build on Hostinger

--- frontend/server.js
+++ frontend/server.js
@@ -5,7 +5,7 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
 const port = process.env.PORT || 3000
-const app = next({ dev, hostname, port })
+const app = next({ dev, hostname, port, dir: __dirname })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
